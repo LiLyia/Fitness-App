876c774a90da8d541943b09f6a9b712a28f869db
Wang Weiao <weiao.wang@yahoo.com>
2024-05-18 10:32:50 +0200

    updated changes

--- app/src/main/java/com/example/fitnessapp/calendar/EventAdapter.java
+++ app/src/main/java/com/example/fitnessapp/calendar/EventAdapter.java
@@ -1,22 +1,29 @@
 package com.example.fitnessapp.calendar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.fitnessapp.R;
+import com.example.fitnessapp.account.UserActivity;
 import com.example.fitnessapp.entity.Event;
 import com.example.fitnessapp.util.CalendarUtils;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class EventAdapter extends ArrayAdapter<Event> {
+
     public EventAdapter(@NonNull Context context, List<Event> events)
     {
         super(context, 0, events);
@@ -31,9 +38,26 @@ public class EventAdapter extends ArrayAdapter<Event> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_cell, parent, false);
 
         TextView eventCellTV = convertView.findViewById(R.id.eventCellTV);
+        Button editButton = convertView.findViewById(R.id.edit);
+        Button deleteButton = convertView.findViewById(R.id.delete);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event.eventsList.clear();
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event.eventsList.clear();
+            }
+        });
 
         String eventTitle = event.getName() +" "+ CalendarUtils.formattedTime(event.getTime());
         eventCellTV.setText(eventTitle);
         return convertView;
     }
+
 }
--- app/src/main/java/com/example/fitnessapp/calendar/WeekViewActivity.java
+++ app/src/main/java/com/example/fitnessapp/calendar/WeekViewActivity.java
@@ -7,10 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -86,10 +91,12 @@ public class WeekViewActivity extends AppCompatActivity implements CalendarAdapt
                 .filter(e -> e.getUsername().equals(usernameText))
                 .collect(Collectors.toCollection(ArrayList::new));
         EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEventsOfCurrentUser);
+
         eventListView.setAdapter(eventAdapter);
     }
 
     public void newEventAction(View view) {
         startActivity(new Intent(this, EventEditActivity.class));
     }
+
 }
